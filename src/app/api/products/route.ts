47efd1b8be5590eabb79a6a66942679ef5163387
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-04-16 16:34:25 -0300

    feat: complete crawlers and integrate to api response

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,13 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getProductsFromCategory } from "@/services/crawler"
+import { UrlParams, getProductsFromCategory } from "@/services/crawler"
 
 export async function GET(request: NextRequest) {
-  const { category, query } = Object.fromEntries(request.nextUrl.searchParams.entries());
-  // const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  // const categories = await response.json();
-  const products = await getProductsFromCategory("mercadoLivre", "geladeira", "");
-  // const products = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${category}&q=${query}`);
+  const { source, category, query } = Object.fromEntries(request.nextUrl.searchParams.entries()) as UrlParams;
+  const products = await getProductsFromCategory(source, category, query);
 
-  // return NextResponse.json({ ...categories });
   return NextResponse.json({ ...products });
 }
--- src/services/crawler.ts
+++ src/services/crawler.ts
@@ -3,14 +3,20 @@ import * as cheerio from 'cheerio';
 const urls = {
   mercadoLivre: {
     geladeira: 'https://lista.mercadolivre.com.br/eletrodomesticos/refrigeracao/geladeira/',
-    tv: 'https://lista.mercadolivre.com.br/eletronicos-audio-video/televisores/',
-    celular: 'https://lista.mercadolivre.com.br/celulares-telefones/celulares-smartphones/',
+    tv: 'https://lista.mercadolivre.com.br/eletronicos-audio-video/tv/',
+    celular: 'https://lista.mercadolivre.com.br/celulares-telefones/celular/',
   },
   buscape: {
-    geladeira: '',
-    tv: '',
-    celular: '',
+    geladeira: 'https://www.buscape.com.br/geladeira',
+    tv: 'https://www.buscape.com.br/tv',
+    celular: 'https://www.buscape.com.br/celular',
   }
+}
+
+export type UrlParams = {
+  source: keyof typeof urls | 'all';
+  category: keyof typeof urls[keyof typeof urls];
+  query: string;
 }
 
 type Product = {
@@ -20,8 +26,32 @@ type Product = {
   link: string;
 }
 
-async function scrapeMercadoLivre(category: keyof typeof urls[keyof typeof urls], query: string) {
-  const response = await fetch(urls.mercadoLivre[category]);
+async function scrapeBuscape(category: UrlParams['category'], query: UrlParams['query']) {
+  let url = urls.buscape[category];
+  if (query) {
+    url = `https://www.buscape.com.br/search?q=${query}`
+  }
+
+  const response = await fetch(url);
+  const page = await response.text();
+  const $ = cheerio.load(page);
+
+  const productCards: Product[] = []
+
+  $('div.Hits_Wrapper__3q_7P').find('div.Paper_Paper__HIHv0').each((_i, elem) => {
+    const image = $(elem).find('div.SearchCard_ProductCard_Image__ffKkn').toString().match(/(?<=src=")[^"]*/g)?.at(-1) as keyof Product;
+    const title = $(elem).find('[data-testid="product-card::name"]').text();
+    const price = $(elem).find('[data-testid="product-card::price"]').text().replace('R$ ', '');
+    const link = `https://www.buscape.com.br${$(elem).find('a.SearchCard_ProductCard_Inner__7JhKb').attr('href')}` as keyof Product;
+    
+    productCards.push({ image, title, price, link });
+  });
+
+  return productCards;
+}
+
+async function scrapeMercadoLivre(category: UrlParams['category'], query: UrlParams['query']) {
+  const response = await fetch(`${urls.mercadoLivre[category]}/${query}`);
   const page = await response.text();
   const $ = cheerio.load(page);
 
@@ -30,7 +60,7 @@ async function scrapeMercadoLivre(category: keyof typeof urls[keyof typeof urls]
   $('ol.ui-search-layout').find('li').each((_i, elem) => {
     const image = $(elem).find('img.ui-search-result-image__element').data('src') as keyof Product;
     const title = $(elem).find('h2.ui-search-item__title').text();
-    const price = $(elem).find('div.ui-search-price__second-line').find('span.price-tag-fraction').eq(0).text();
+    const price = `${$(elem).find('div.ui-search-price__second-line').find('span.price-tag-fraction').eq(0).text()},00`;
     const link = $(elem).find('a.ui-search-link').attr('href') as keyof Product;
 
     productCards.push({ image, title, price, link });
@@ -39,8 +69,13 @@ async function scrapeMercadoLivre(category: keyof typeof urls[keyof typeof urls]
   return productCards;
 }
 
-export async function getProductsFromCategory(source: keyof typeof urls, category: keyof typeof urls[keyof typeof urls], query: string) {
+export async function getProductsFromCategory(source: UrlParams['source'], category: UrlParams['category'], query: UrlParams['query']) {
   if (source === 'mercadoLivre') {
     return scrapeMercadoLivre(category, query);
   }
+  if (source === 'buscape') {
+    return scrapeBuscape(category, query);
+  }
+  const [ mercadoLivreProducts, buscapeProducts ] = await Promise.all([scrapeMercadoLivre(category, query), scrapeBuscape(category, query)]);
+  return [...mercadoLivreProducts, ...buscapeProducts];
 }
